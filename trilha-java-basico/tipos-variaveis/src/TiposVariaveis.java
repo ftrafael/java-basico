4ce57efad11e4a7ab3a9cd09ddaf540e986032a9
Rafael <rft.ferreira@gmail.com>
2023-01-12 13:44:01 -0300

    tipos e variaveis finalizado.

--- trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
+++ trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
@@ -4,6 +4,11 @@ public class TiposVariaveis {
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
-        short numeroCurto2 = numeroNormal;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // uma conts em java inicia sempre com a expressão "final" seguida do tipo  e o nome todo em maiusculo (ex.: VALOR_DE_PI)
+
+        final double VALOR_DE_PI = 3.14;
+        // VALOR_DE_PI = 10.75; não é possível mudar.
     }
 }
