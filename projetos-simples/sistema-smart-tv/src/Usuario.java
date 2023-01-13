51d23af8acbf6e01e0de70e970cd6cde8d84266b
Rafael <rft.ferreira@gmail.com>
2023-01-13 11:34:26 -0300

    SmartTv, classes e metodos criados.

---
+++ projetos-simples/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,30 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " +smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status-> TV Ligada? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.diminuirCanal();
+        smartTv.mudarCanal(13);
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+
+        smartTv.desligar();
+        System.out.println("Novo Status-> TV Ligada? " + smartTv.ligada);
+
+
+    }
+}
